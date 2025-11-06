a98056fe947967f18c43e2aefa85da26a94f56b4
BlankoTheLegend <zac.louca3@gmail.com>
2025-11-06 09:23:57 +0000

    Refactor service worker caching logic

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,9 @@ const CACHE_NAME = 'workout-cache-v1';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json',
   './style.css',
   './script.js',
+  './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
